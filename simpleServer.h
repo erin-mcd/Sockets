0ce8d01685cbcec716ea3639f47d1c16600011bc
Erin McDonald <tug94529@temple.edu>
2019-11-11 00:50:19 -0500

    huge improvement

--- simpleServer.h
+++ simpleServer.h
@@ -8,6 +8,33 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <unistd.h>
+#include <limits.h>
+#include <pthread.h>
 #define BUF_LEN 512
+#define DICTIONARY_SIZE 99171
+#define Q_SIZE 10
+
+struct Queue
+{
+	// first in first out
+	int size;
+	int capacity;
+	int first;
+	int last;
+	int* store;
+};
+
+int isFull(struct Queue *q);
 int open_listenfd(int);
+void put(int socket, struct Queue* workQ);
+int get(struct Queue* workQ);
+void *worker(void *arg);
+void initQueue(struct Queue* q, int capacity);
+void enqueue(int socket , struct Queue* q);
+int size(struct Queue* q);
+int dequeue(struct Queue* q);
+int isEmpty(struct Queue *q);
+void service(char *wordArray[], int clientSocket);
+void printQ(struct Queue* q);
+
 #endif
